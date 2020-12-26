03dfec3d3aa535a7176363e7e71c59ffa1fcd848
Sam Levy <slevy239@gmail.com>
2020-12-26 13:50:05 -0500

    Updates

---
+++ precache-manifest.5f623094b0a23245031bc187b3fea2f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df5557d99c473fe0fe8655a0f10cbbf",
+    "url": "/C9C/index.html"
+  },
+  {
+    "revision": "3794c4db3f468c7e7ca6",
+    "url": "/C9C/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ce1a981ba3e3a74c53af",
+    "url": "/C9C/static/js/2.9965b8e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/C9C/static/js/2.9965b8e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3794c4db3f468c7e7ca6",
+    "url": "/C9C/static/js/main.7212479e.chunk.js"
+  },
+  {
+    "revision": "a04216c5c35091c1fd67",
+    "url": "/C9C/static/js/runtime-main.b410ce85.js"
+  }
+]);
